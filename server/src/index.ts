c5189df519229c3a3d55b2825e3bcb8ae8a27a38
deleb <penetra.okulo@gmail.com>
2019-09-19 08:33:16 +0900

    basic setting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,23 @@
-import "./importThisFirstPlease";
+import Koa from "koa";
+import router from "./router";
+import koaBody from "koa-body";
 
-console.log("hello world123");
+const app = new Koa();
+const PORT: number = process.env.NODE_ENV === "production" ? 4001 : 4002;
+
+app.use(
+  koaBody({
+    multipart: true,
+    formidable: {
+      uploadDir: __dirname + "/uploads",
+    },
+  }),
+);
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log(`server is listening to port ${PORT}`);
+});
+
+export default app;
---
+++ server/src/router.ts
@@ -0,0 +1,7 @@
+import Koa from "koa";
+import Router from "koa-router";
+import * as fs from "fs";
+
+const router = new Router();
+
+export default router;
