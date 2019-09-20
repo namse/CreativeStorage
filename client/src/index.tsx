192d99afddb82dd6958a7ff16603fc67d6229640
iwannadevops <dudwls3320@gamil.com>
2019-09-20 20:52:05 +0900

    Modigy index.tsz

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { render } from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById("root"),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
