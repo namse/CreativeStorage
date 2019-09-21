d5857098ddccab2f137c834ceee91ea4da339e93
Deleb <penetra.okulo@gmail.com>
2019-09-21 18:47:23 +0900

    replace fs with filename created before upload in test

--- server/src/test/uploadAndGetFileMetadataList.test.ts
+++ server/src/test/uploadAndGetFileMetadataList.test.ts
@@ -19,31 +19,16 @@ async function getFileMetadataList(): Promise<FileMetadata[]> {
   return result;
 }
 
-function readDir(uploadsDirectory: string, resolve: any, reject: any) {
-  fs.readdir(uploadsDirectory, (err, data) => {
-    if (err) {
-      reject(err);
-      return;
-    }
-    resolve(data);
-  });
-}
-
 it("getlist file", async () => {
   const imageInBase64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==";
   const imageBuffer = Buffer.from(imageInBase64, "base64");
   const filename = uuid();
   await uploadFile(filename, imageBuffer);
 
-  const apiFileMetadataList = await getFileMetadataList();
-  const filenameFromApi = apiFileMetadataList.map((el: FileMetadata) => {
+  const fileMetadataList = await getFileMetadataList();
+  const filenameFromApi = fileMetadataList.map((el: FileMetadata) => {
     return el.filename;
   });
 
-  const filenameFromFs = await new Promise((resolve, reject) => {
-    const uploadsDirectory = path.join(__dirname, "..", "..", "/dist/uploads");
-    readDir(uploadsDirectory, resolve, reject);
-  });
-
-  expect(filenameFromApi).toEqual(filenameFromFs);
+  expect(filenameFromApi).toEqual(expect.arrayContaining([filename]));
 });
