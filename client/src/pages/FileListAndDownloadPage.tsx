b73030f95ac621b30693be169ca73a131247cab1
HANJIN-O <zeros0623@gmail.com>
2019-09-23 17:25:15 +0900

    ing

---
+++ client/src/pages/FileListAndDownloadPage.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import FileListComponent from "src/components/FileListComponent";
+
+export default class FileListAndDownloadPage extends React.Component {
+  public render() {
+    <div></div>;
+  }
+}
