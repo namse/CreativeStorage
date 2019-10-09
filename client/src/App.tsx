b2acdebfabcc9462ca8d1fe8d2edb3cfa5793579
HANJIN-O <zeros0623@gmail.com>
2019-10-09 20:25:59 +0900

    Remove router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,12 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "src/pages/HomePage";
-import UploadFilePage from "src/pages/UploadFilePage";
-import DownloadFilePage from "src/pages/DownloadFilePage";
 
 export default class App extends React.Component {
   public render() {
     return (
-      <Router>
-        <div>
-          <Route path="/" component={HomePage} />
-          <Route path="/upload" component={UploadFilePage} />
-          <Route path="/download" component={DownloadFilePage} />
-        </div>
-      </Router>
+      <div>
+        <HomePage />
+      </div>
     );
   }
 }
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,15 +1,66 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import UploadFilePage from "src/pages/UploadFilePage";
+import DownloadFilePage from "src/pages/DownloadFilePage";
 
-const HomePage = () => (
-  <div>
-    <Link to={"/Download"}>
-      <button>Download</button>
-    </Link>
-    <Link to={"/Upload"}>
-      <button>Upload</button>
-    </Link>
-  </div>
-);
+type HomePagePropsType = {};
+type FileWithTransferState = { file: File; transfered: boolean };
+type State = {
+  files: FileWithTransferState[];
+  componentToBeRendered: string;
+};
 
-export default HomePage;
+export default class HomePage extends React.Component<
+  HomePagePropsType,
+  State
+> {
+  public constructor(props: HomePagePropsType) {
+    super(props);
+    this.state = {
+      files: [],
+      componentToBeRendered: "",
+    };
+  }
+
+  public render() {
+    const visible = {
+      display: "none",
+    };
+    const invisible = {};
+    return (
+      <div>
+        <button onClick={() => this.handleClickedButtonDownload()}>
+          Download
+        </button>
+        <button onClick={() => this.handleClickedButtonUpload()}>Upload</button>
+        <div className="download-page" style={invisible}>
+          <DownloadFilePage />
+        </div>
+        <div className="upload-page" style={visible}>
+          <UploadFilePage />
+        </div>
+      </div>
+    );
+  }
+
+  private handleClickedButtonDownload() {
+    const downloadComponent = document.querySelector(".download-page");
+    const uploadComponent = document.querySelector(".upload-page");
+    if (downloadComponent !== null) {
+      downloadComponent.setAttribute("style", "");
+    }
+    if (uploadComponent !== null) {
+      uploadComponent.setAttribute("style", "display: none;");
+    }
+  }
+
+  private handleClickedButtonUpload() {
+    const downloadComponent = document.querySelector(".download-page");
+    const uploadComponent = document.querySelector(".upload-page");
+    if (downloadComponent !== null) {
+      downloadComponent.setAttribute("style", "display: none;");
+    }
+    if (uploadComponent !== null) {
+      uploadComponent.setAttribute("style", "");
+    }
+  }
+}
