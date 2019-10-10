df25587c02e74312f5d609afe640b107b3b77ca3
yobs0814 <yobs0814@gmail.com>
2019-10-10 19:14:57 +0900

    Add uploadurl and downloadurl test

--- server/src/test/serverless.test.ts
+++ server/src/test/serverless.test.ts
@@ -1,5 +1,11 @@
 import { start, stop } from "../ServerlessUtil";
 import fetch from "node-fetch";
+import uuid = require("uuid");
+import { generateTestBlob } from "../../../client/src/test/browserTest/FileManager.browsertest";
+import { envModule } from "../config/.env";
+import { fileMetadata } from "src/IStorageService";
+
+const endpoint: string = envModule.LOCALTESTIP;
 
 export async function getTest(testUrl: string): Promise<boolean> {
   const response = await fetch(testUrl);
@@ -10,8 +16,14 @@ export async function getTest(testUrl: string): Promise<boolean> {
   return response.ok;
 }
 
+async function getFileMetadataList(): Promise<fileMetadata[]> {
+  const response = await fetch(endpoint + "/filemetadatalist");
+  const fileMetadataList = await response.json();
+  return fileMetadataList;
+}
+
 async function healthCheck(): Promise<boolean> {
-  const url = "http://localhost:4002/";
+  const url = endpoint;
 
   const response = await fetch(url);
   let status: boolean = false;
@@ -38,8 +50,31 @@ describe("servereless", () => {
   it("/filemetadatalist api get request response test", async () => {
     const serverStatus: boolean = await healthCheck();
     if (serverStatus) {
-      const status: boolean = await getTest("http://localhost:4002/filemetadatalist");
+      const status: boolean = await getTest(`${endpoint}/filemetadatalist`);
       expect(status).toEqual(true);
+    } else {
+      expect(true).toEqual(false);
+    }
+  });
+
+  it("/uploads api get request response test", async () => {
+    const serverStatus: boolean = await healthCheck();
+    if (serverStatus) {
+      const filename = `${123456789}.txt`;
+      const response = await fetch(`${endpoint}/uploadfileurl?filename=${filename}&contentType=text/plain`);
+      expect(response.status).toEqual(200);
+    } else {
+      expect(true).toEqual(false);
+    }
+  });
+
+  it("/downloads api get request response test", async () => {
+    const serverStatus: boolean = await healthCheck();
+    if (serverStatus) {
+      const fileMetadataList: fileMetadata[] = await getFileMetadataList();
+      const filename: string = fileMetadataList[0].key;
+      const response = await fetch(encodeURI(`${endpoint}/downloadfileurl?filename=${(filename)}`));
+      expect(response.status).toEqual(200);
     } else {
       expect(true).toEqual(false);
     }
