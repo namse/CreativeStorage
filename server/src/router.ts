60a67f8a1d3f4e411a0a2da454651448096f9f58
leaveittogod0123 <yobs0814@gmail.com>
2019-10-10 22:54:23 +0900

    Set up Serverless for deployment (#35)
    * Convert koa app to aws lambda by servereless framework
    
    * Add uploadurl and downloadurl test
    
    * Fix downloadurl test
    
    * Remove unused import
    
    * Implement serverless except aws key

--- server/src/router.ts
+++ server/src/router.ts
@@ -20,6 +20,10 @@ export default function applyRouter(
   controller.router.get("/deletefileurl", (ctx: Koa.Context) =>
     controller.getUrlForDeleteFile(ctx),
   );
+  controller.router.get("/health", (ctx: Koa.Context) => {
+    ctx.body = "OK";
+    ctx.status = 200;
+  });
 
   app.use(controller.router.routes());
   app.use(controller.router.allowedMethods());
